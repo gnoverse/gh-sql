49edd99b27d02053d7dadfd321140a61e1b9bfb5
Morgan Bazalgette <morgan@morganbaz.com>
2025-01-25 15:29:20 +0100

    refactor(sync): split resource fetching into different packages

---
+++ pkg/sync/pulls.go
@@ -0,0 +1,156 @@
+package sync
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/gnolang/gh-sql/ent"
+	"github.com/gnolang/gh-sql/ent/issue"
+	"github.com/gnolang/gh-sql/ent/pullrequest"
+	"github.com/gnolang/gh-sql/ent/repository"
+	"github.com/gnolang/gh-sql/pkg/model"
+	"github.com/gnolang/gh-sql/pkg/sync/internal/synchub"
+	"golang.org/x/sync/errgroup"
+)
+
+// fetchPullRequest can be used to retrive an issue, knowing its repo and issue number.
+type fetchPullRequest struct {
+	owner      string
+	repo       string
+	pullNumber int64
+}
+
+var _ synchub.Resource[*ent.PullRequest] = fetchPullRequest{}
+
+func (fi fetchPullRequest) ID() string {
+	return fmt.Sprintf("/repos/%s/%s/pulls/%d",
+		fi.owner, fi.repo, fi.pullNumber)
+}
+
+func (fi fetchPullRequest) Fetch(ctx context.Context, h *synchub.Hub) (*ent.PullRequest, error) {
+	var pr pullAndEdges
+	if err := synchub.Get(ctx, h, fi.ID(), &pr); err != nil {
+		return nil, fmt.Errorf("fetchPullRequest%+v get: %w", fi, err)
+	}
+	return fi.fetch(ctx, h, pr)
+}
+
+type pullAndEdges struct {
+	ent.PullRequest
+
+	User               *model.SimpleUser  `json:"user"`
+	Assignees          []model.SimpleUser `json:"assignees"`
+	RequestedReviewers []model.SimpleUser `json:"requested_reviewers"`
+}
+
+func (fi fetchPullRequest) fetch(ctx context.Context, h *synchub.Hub, pr pullAndEdges) (*ent.PullRequest, error) {
+	cr := h.DB.PullRequest.Create().CopyPullRequest(&pr.PullRequest)
+
+	eg, _ := errgroup.WithContext(ctx)
+	iss, err := h.DB.Issue.Query().
+		Where(
+			issue.HasRepositoryWith(repository.FullName(fi.owner+"/"+fi.repo)),
+			issue.Number(pr.Number),
+		).
+		Only(ctx)
+	if err != nil && !ent.IsNotFound(err) {
+		return nil, err
+	}
+	if iss == nil || iss.UpdatedAt.Before(pr.UpdatedAt) {
+		// Fetch issue.
+		eg.Go(func() error {
+			iss, err := synchub.Fetch(ctx, h, fetchIssue{fi.owner, fi.repo, fi.pullNumber})
+			if err != nil {
+				return err
+			}
+			cr.SetIssueID(iss.ID)
+			return nil
+		})
+	} else {
+		cr.SetIssueID(iss.ID)
+	}
+	eg.Go(func() error {
+		// Fetch repository and set repo ID.
+		repo, err := synchub.Fetch(ctx, h, fetchRepository{fi.owner, fi.repo})
+		if err != nil {
+			return err
+		}
+		cr.SetRepositoryID(repo.ID)
+		return nil
+	})
+	var setterLock sync.Mutex
+	localGetUser := func(login string, setter func(id int64) *ent.PullRequestCreate) {
+		if login == "" {
+			return
+		}
+		eg.Go(func() error {
+			// Fetch creator
+			user, err := synchub.Fetch(ctx, h, fetchUser{login})
+			if err != nil {
+				return err
+			}
+			setterLock.Lock()
+			defer setterLock.Unlock()
+			setter(user.ID)
+			return nil
+		})
+	}
+	if pr.User != nil {
+		localGetUser(pr.User.Login, cr.SetUserID)
+	}
+	for _, assignee := range pr.Assignees {
+		login := assignee.Login
+		localGetUser(login, func(i int64) *ent.PullRequestCreate { return cr.AddAssigneeIDs(i) })
+	}
+	for _, reviewer := range pr.RequestedReviewers {
+		login := reviewer.Login
+		localGetUser(login, func(i int64) *ent.PullRequestCreate { return cr.AddRequestedReviewerIDs(i) })
+	}
+
+	err = eg.Wait()
+	if err != nil {
+		return nil, fmt.Errorf("fetchPullRequest%+v fetch deps: %w", fi, err)
+	}
+
+	err = cr.OnConflict().UpdateNewValues().
+		Exec(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("fetchPullRequest%+v save: %w", fi, err)
+	}
+
+	return h.DB.PullRequest.Get(ctx, pr.ID)
+}
+
+func fetchPulls(ctx context.Context, h *synchub.Hub, repoOwner, repoName string) {
+	iter := synchub.GetIterate[pullAndEdges](
+		ctx, h,
+		fmt.Sprintf("/repos/%s/%s/pulls?state=all&per_page=100", repoOwner, repoName))
+	for pr := range iter.Values {
+		oldPR, err := h.DB.PullRequest.Query().
+			Select(pullrequest.FieldUpdatedAt).
+			Where(pullrequest.ID(pr.ID)).
+			Only(ctx)
+		if err != nil && !ent.IsNotFound(err) {
+			h.Warn(err)
+			continue
+		}
+
+		if oldPR == nil || !oldPR.UpdatedAt.Equal(pr.UpdatedAt) {
+			// this does not incur in an additional request; we have all the data
+			// we want from this request already
+			fi := fetchPullRequest{repoOwner, repoName, pr.Number}
+			fn, created := synchub.SetUpdatedFunc(h, fi.ID(), func() (*ent.PullRequest, error) {
+				return fi.fetch(ctx, h, pr)
+			})
+			if created {
+				if _, err := fn(); err != nil {
+					h.Warn(err)
+				}
+			}
+		}
+	}
+	if iter.Err != nil {
+		h.Warn(fmt.Errorf("fetchPulls(%q, %q): %w", repoOwner, repoName, iter.Err))
+	}
+}
--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -3,11 +3,13 @@ package sync
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"regexp"
 	"strings"
 
 	"github.com/gnolang/gh-sql/ent"
+	"github.com/gnolang/gh-sql/pkg/model"
 	"github.com/gnolang/gh-sql/pkg/sync/internal/synchub"
 )
 
@@ -48,4 +50,88 @@ func Sync(ctx context.Context, repositories []string, opts Options) error {
 
 	// wait for all goros to finish
 	return h.Wait()
+}
+
+// fetchRepository can be used to fetch a GitHub repository, knowing its
+// owner and name
+type fetchRepository struct {
+	owner, repo string
+}
+
+var _ synchub.Resource[*ent.Repository] = fetchRepository{}
+
+func (f fetchRepository) ID() string { return "/repos/" + f.owner + "/" + f.repo }
+
+func (f fetchRepository) Fetch(ctx context.Context, h *synchub.Hub) (*ent.Repository, error) {
+	var r struct {
+		ent.Repository
+		Owner model.SimpleUser `json:"owner"`
+	}
+	if err := synchub.Get(ctx, h, f.ID(), &r); err != nil {
+		return nil, fmt.Errorf("fetchRepository%+v fetch: %w", f, err)
+	}
+
+	u, err := synchub.Fetch(ctx, h, fetchUser{username: r.Owner.Login})
+	if err != nil {
+		return nil, err
+	}
+
+	err = h.DB.Repository.Create().
+		CopyRepository(&r.Repository).
+		SetOwnerID(u.ID).
+		OnConflict().UpdateNewValues().
+		Exec(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("fetchRepository%+v save: %w", f, err)
+	}
+
+	h.Go(func() error {
+		// Pulls are fetched after issues, so that pulls already have all of the
+		// PRs they link to fetched into the database.
+		fetchIssues(ctx, h, r.Owner.Login, r.Name)
+		fetchPulls(ctx, h, r.Owner.Login, r.Name)
+		return nil
+	})
+
+	return h.DB.Repository.Get(ctx, r.ID)
+}
+
+func fetchRepositories(ctx context.Context, h *synchub.Hub, owner string, shouldFetch func(*ent.Repository) bool) {
+	iter := synchub.GetIterate[*ent.Repository](
+		ctx, h,
+		fmt.Sprintf("/users/%s/repos?per_page=100", owner))
+	for r := range iter.Values {
+		if shouldFetch(r) {
+			synchub.FetchAsync(ctx, h, fetchRepository{owner, r.Name})
+		}
+	}
+	if iter.Err != nil {
+		h.Warn(fmt.Errorf("fetchRepositories(%q): %w", owner, iter.Err))
+	}
+}
+
+// fetchUser can be used to retrive a user, knowing its username.
+type fetchUser struct {
+	username string
+}
+
+var _ synchub.Resource[*ent.User] = fetchUser{}
+
+func (fu fetchUser) ID() string { return "/users/" + fu.username }
+
+func (fu fetchUser) Fetch(ctx context.Context, h *synchub.Hub) (*ent.User, error) {
+	var u ent.User
+	if err := synchub.Get(ctx, h, fu.ID(), &u); err != nil {
+		return nil, fmt.Errorf("fetchUser%+v get: %w", fu, err)
+	}
+
+	err := h.DB.User.Create().
+		CopyUser(&u).
+		OnConflict().UpdateNewValues().
+		Exec(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("fetchUser%+v save: %w", fu, err)
+	}
+
+	return h.DB.User.Get(ctx, u.ID)
 }
